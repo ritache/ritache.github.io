c8d6498f349e950367e0ea660606648db94b1450
ritache <ritachekhovskaya@gmail.com>
2019-08-15 17:57:08 -0400

    Kaus Case Study

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,32 @@
 // ensure navbar doesn't cover scroll to section
-$('#chevron').click(function(){    
-    var divId = $(this).attr('href');
-     $('html, body').animate({
-      scrollTop: $(divId).offset().top - 80
-    }, 100);
-  });
+$('.anchor').click(function(){    
+  var divId = $(this).attr('href');
+   $('html, body').animate({
+    scrollTop: $(divId).offset().top - 80
+  }, 100);
+});
 
-  // disable dragging and saving of images
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollTop").style.display = "block";
+  } else {
+    document.getElementById("scrollTop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//   // disable dragging and saving of images
+//   document.oncontextmenu = function(e){
+//     var target = (typeof e !="undefined")? e.target: event.srcElement
+//     if (target.tagName == "IMG" || (target.tagName == 'A' && target.firstChild.tagName == 'IMG'))
+//         return false
+// }
+// add to body tag if needed on contextmenu="return false;" onselectstart="return false;" ondragstart="return false;"
